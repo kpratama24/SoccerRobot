c990db40e265e373f58126719dfebcc05f15e8ba
Kevin Pratama <7314073@student.unpar.ac.id>
2017-10-08 22:01:35 +0700

    Add exit confirmation

--- app/src/main/java/com/nearkevin/soccerrobot/MainActivity.java
+++ app/src/main/java/com/nearkevin/soccerrobot/MainActivity.java
@@ -1,11 +1,13 @@
 package com.nearkevin.soccerrobot;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button startButton;
     private TextView aboutText;
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         aboutText.setOnClickListener(this);
     }
 
+    @Override
+    public void onBackPressed() {
+        if(doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
 
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press again to exit",Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        },2000);
+    }
 
     @Override
     public void onClick(View view) {
